f22b46fa4c3c24d4ddbc2f4112eab4b8ee6b4fd5
demetdevrik <demettdevrik@gmail.com>
2026-02-11 15:51:29 +0300

    Finished the decoder

--- morse/decoder.py
+++ morse/decoder.py
@@ -1,9 +1,24 @@
-pass  # YOUR CODE HERE
+from morse.mapping import MORSE
+
+REVERSE_MORSE = {value: key for key, value in MORSE.items()}
 
 
-if __name__ == "__main__":
-    # Example usage for one word
-    pass  # YOUR CODE HERE
+def decode_word(morse_word):
+    letters = morse_word.split()
+    decoded_letters = []
 
-    # Example usage for one sentence
-    pass  # YOUR CODE HERE
+    for letter in letters:
+        if letter in REVERSE_MORSE:
+            decoded_letters.append(REVERSE_MORSE[letter])
+
+    return "".join(decoded_letters)
+
+
+def decode(morse_code):
+    words = morse_code.split("|")
+    decoded_words = []
+
+    for word in words:
+        decoded_words.append(decode_word(word))
+
+    return " ".join(decoded_words)
